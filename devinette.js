cf39541eec617fa6b598adf1ba8249b3df90ed44
Vincent MATIAS <vincent77.matias77@gmail.com>
2023-09-26 00:14:19 +0200

    Ajout des fichier de base

---
+++ devinette.js
@@ -0,0 +1,29 @@
+// Générer un nombre aléatoire
+//Donner à l'utilisateur la capacité de deviner/jouer
+//Faire deviner l'utilisateur et s'il a faux, on lui fait recomencer le jeu, on lui repose la question
+//S'il gagne, on stop
+
+
+//function game() {
+//  let randomNbr = Math.floor(Math.random() * 10);
+//    let nbr;
+
+//    do {
+//        nbr = prompt('Devine un nombre entre 1 et 10');
+  //      alert(nbr, randomNbr)
+
+   //     if(randomNbr > nbr){
+    //        alert("Trop petit")
+    //    } else if (randomNbr > nbr){
+    //        alert("Trop grand")
+    //    }
+ //   }
+ //   while(nbr != randomNbr);
+//}
+//game();
+
+
+
+
+
+// PROJET ABANDONNEE
